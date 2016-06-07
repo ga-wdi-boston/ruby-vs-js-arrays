e4b366cd283db9b42040cdf08bc960cedbd79b5f
berziiii <brian@offpistedesigns.com>
2016-06-07 11:04:53 -0400

    solution for code_along

--- bin/code_along.rb
+++ bin/code_along.rb
@@ -1,7 +1,13 @@
 
 # Create an empty array, `lit_array`, using the literal syntax
+lit_array = Array.new
+
+#or lit_array = []
 
 # Create an array, `constr_array`, using the constructor syntax, initialized
 # with 5 elements
+constr_array = Array.new(5)
 
 # Create an array, `ten_false_array`, initialized with 10 `false` elements
+
+ten_false_array = Array.new(10, false)
